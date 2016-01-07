fd28d97d58e26643c9d9493ec5f666e0d5fda411
Brandon Gonzalez <brandon.gonzalez.451@gmail.com>
2016-01-07 15:06:39 -0500

    caching ftw

--- web.js
+++ web.js
@@ -1,8 +1,14 @@
 var express = require('express');
+var jade = require('jade');
 var app = express();
 
+var options = {cache: true};
+jade.compileFile('./views/index.jade', options);
+
+app.set('view cache', true);
 app.set('views', './views');
 app.set('view engine', 'jade');
+
 app.use(express.static('public'));
 app.get('/', function(req, res) {
   res.render('index');
