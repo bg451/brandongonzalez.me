4331d27baf4036717f455d13119a162de9180f50
Brandon Gonzalez <brandon.gonzalez.451@gmail.com>
2016-01-07 15:08:51 -0500

    pings website to keep heroku alive

--- web.js
+++ web.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var jade = require('jade');
+var http = require('http');
 var app = express();
 
 var options = {cache: true};
@@ -8,10 +9,13 @@ jade.compileFile('./views/index.jade', options);
 app.set('view cache', true);
 app.set('views', './views');
 app.set('view engine', 'jade');
-
 app.use(express.static('public'));
 app.get('/', function(req, res) {
   res.render('index');
 });
+
+setInterval(function() {
+  http.get("http://brandongonzalez.me");
+}, 300000); // every 5 minutes (300000)
 
 app.listen(process.env.PORT || 3000)
